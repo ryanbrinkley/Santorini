c1ec2aec6b8cb16d679d4559e85abc2c0c52851f
mbrinkl <bjbrinkleyyy@yahoo.com>
2018-10-17 05:40:17 -0500

    Update Character.py

--- Character.py
+++ Character.py
@@ -1,53 +1,36 @@
+characterList = [
+    "Apollo", 
+    "Artemis", 
+    "Hecate", 
+    "Zeus"
+]
+
 class Character:
-
-    def __init__(self, x, y):
-        self.position = (x, y)
+    def __init__(self):
+        self.workers = []
+        self.numWorkers = 2
+        self.selectedWorker = None  
         self.isTurn = False
-        self.buildRange = 1
-        self.height = 0
 
-    def move(self, x, y):
-        self.position = (x, y)
+class Worker:
+    def __init__(self, id, space):
+        self.space = space
+        self.pos = space.pos
+        self.id = id
+
+    def move(self, pos):
+        self.space = pos
         # make board spaces unavailable / check build availables
 
-    def build(self, board, x, y):
-        board.build(x, y)
-        # make board spaces unavailable
-
-    def check_available_moves(self, board):
-        x = self.position[0]
-        y = self.position[1]
-        board.make_available(x - 1, y - 1)
-        board.make_available(x, y - 1)
-        board.make_available(x + 1, y - 1)
-        board.make_available(x - 1, y)
-        board.make_available(x + 1, y)
-        board.make_available(x - 1, y + 1)
-        board.make_available(x, y + 1)
-        board.make_available(x + 1, y + 1)
-
-    def check_available_builds(self, board):
-        x = self.position[0]
-        y = self.position[1]
-        board.make_available(x - 1, y - 1)
-        board.make_available(x, y - 1)
-        board.make_available(x + 1, y - 1)
-        board.make_available(x - 1, y)
-        board.make_available(x + 1, y)
-        board.make_available(x - 1, y + 1)
-        board.make_available(x, y + 1)
-        board.make_available(x + 1, y + 1)
-
 class Apollo(Character):
-
-    def __init__(self, x, y):
-        super(x, y)
+    def __init__(self):
+        super(Apollo, self).__init__()
+        self.name = "Apollo"
         self.description = "You may move into an opponent's square and trade places" \
                            " with him"
 
-
 class Artemis(Character):
-
-    def __init__(self, x, y):
-        super(x, y)
+    def __init__(self):
+        super(Artemis, self).__init__()
+        self.name = "Artemis"
         self.description = "Queer as a Steer"
