f654721ba3b7746cd2542f83d49ba94ad39d23d2
mbrinkl <bjbrinkleyyy@yahoo.com>
2018-10-16 21:35:36 -0500

    Update Board.py

--- Board.py
+++ Board.py
@@ -1,20 +1,33 @@
 class Board:
 
     def __init__(self):
-        self.spaces = [[Space() for i in range(5)] for i in range(5)]
+        self.spaces = [[Space(i, j) for i in range(5)] for j in range(5)]
 
     def build(self, x, y):
         self.spaces[x][y].increment_size()
 
-    # def check_available_move(self):
+    def make_available(self, x, y):
+        if x >= 0 and x <= 5 and y >= 0 and y <= 5:
+            # Check for building level
+            # Check for another character
+            self.spaces[x][y].availability = True
+
+    def get_available_spaces(self):
+        print("Available Spaces:")
+        for i in range(5):
+            for j in range(5):
+                if self.spaces[i][j].availability == True:
+                    print(self.spaces[i][j].location, end=" ")
+        print()
 
 
 class Space:
 
-    def __init__(self):
+    def __init__(self, x, y):
         # 0 = no building
         # 1,2,3 = size 1,2,3 building
         # 4 = domed building
+        self.location = (x, y)
         self.buildingSize = 0
         self.maxBuildingSize = 4
         self.availability = False
