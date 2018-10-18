c31994a0abd60ab238ecfa4d904e96733d830787
mbrinkl <bjbrinkleyyy@yahoo.com>
2018-10-18 02:44:30 -0500

    Update Character.py

--- Character.py
+++ Character.py
@@ -1,8 +1,14 @@
 characterList = [
     "Apollo", 
     "Artemis", 
-    "Hecate", 
-    "Zeus"
+    "Athena", 
+    "Atlas",
+    "Demeter",
+    "Hephaestus",
+    "Hermes",
+    "Minotaur",
+    "Pan",
+    "Prometheus"
 ]
 
 class Character:
@@ -30,11 +36,110 @@ class Apollo(Character):
     def __init__(self):
         super(Apollo, self).__init__()
         self.name = "Apollo"
-        self.description = "You may move into an opponent's square and trade places" \
-                           " with him"
+        self.difficulty = "Simple"
+        self.description = ("Your Move : Your worker may move into an opponent worker's space by "
+                            "forcing their worker to the space you just vacated.")
+    
+    def ability(self):
+        pass
+        # valid space on opponent chars
+        # if space contains opponent
+        # opponent position = self's old position
+        # make valid_spaces function a character function with possible override?
+        # or just receive validList and alter it
+        # Ability time = Game stage?
 
 class Artemis(Character):
     def __init__(self):
         super(Artemis, self).__init__()
         self.name = "Artemis"
-        self.description = "Queer as a Steer"
+        self.difficulty = "Simple"
+        self.description = ("Your Move: Your worker may move one additional time, but not back to "
+                            "its initial space.")
+
+    def ability(self):
+        pass
+        # after move, allow move again
+        # allow move to first move spot (like only moved once)
+        # remove old position from valid list
+
+class Athena(Character):
+    def __init__(self):
+        super(Athena, self).__init__()
+        self.name = "Athena"
+        self.difficulty = "Simple"
+        self.description = ("Opponent's Turn: If one of your workers moved up on your last turn, "
+                            "opponent workers cannot move up this turn.")
+
+        # opponent cant go up = True if height > prev height else False
+
+class Atlas(Character):
+    def __init__(self):
+        super(Atlas, self).__init__()
+        self.name = "Atlas"
+        self.difficulty = "Simple"
+        self.description = "Your Build: Your worker may build a dome at any level."
+
+        # need to make dome attribute in space
+        # could temporarily set height to 4 on a build
+        # needs option to differentiate between dome build and reg build
+
+class Demeter(Character):
+    def __init__(self):
+        super(Demeter, self).__init__()
+        self.name = "Demeter"
+        self.difficulty = "Simple"
+        self.description = ("Your Build: Your worker may build one additional time, but not on the "
+                            " same space.")
+
+        # after build, allow build again
+        # remove first build spot from valid list
+
+class Hephaestus(Character):
+    def __init__(self):
+        super(Hephaestus, self).__init__()
+        self.name = "Hephaestus"
+        self.difficulty = "Simple"
+        self.description = ("Your Build: Your Worker may build one additional block (not dome) " 
+                            "on top of your first block.")
+        # after build, allow build again
+        # but second's valid list is only on the first spot
+        # needs option to not build twice
+        # no build if height will be dome
+
+class Hermes(Character):
+    def __init__(self):
+        super(Hermes, self).__init__()
+        self.name = "Hermes"
+        self.difficulty = "Simple"
+        self.description = ("Your Turn: If your Workers do not move up or down, they may "
+                            "each move any number of times (even zero), and then either builds")
+
+        # while currheight == height
+        # possibility to move again or not at all
+        # both can build (still in if state)
+
+class Minotaur(Character):
+    def __init__(self):
+        super(Minotaur, self).__init__()
+        self.name = "Minotaur"
+        self.difficulty = "Simple"
+        self.description = ("Your Move: Your Worker may move into an opponent Worker’s space, "
+                            "if their Worker can be forced one space straight backwards to an "
+                            "unoccupied space at any level.")
+
+class Pan(Character):
+    def __init__(self):
+        super(Pan, self).__init__()
+        self.name = "Pan"
+        self.difficulty = "Simple"
+        self.description = ("Win Condition: You also win if your Worker moves down two or "
+                            "more levels.")
+
+class Prometheus(Character):
+    def __init__(self):
+        super(Prometheus, self).__init__()
+        self.name = "Prometheus"
+        self.difficulty = "Simple"
+        self.description = ("Your Turn: If your Worker does not move up, it may build both "
+                            "before and after moving.")
